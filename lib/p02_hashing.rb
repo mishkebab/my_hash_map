5becf9701bdf7345b8f4492c7f6b70e7a63ab382
kennyvngo <kennyvungo@gmail.com>
2023-04-24 14:50:32 -0700

    finished hashing part 2

--- lib/p02_hashing.rb
+++ lib/p02_hashing.rb
@@ -4,11 +4,20 @@ end
 
 class Array
   def hash
+    hashed_str = ''
+    self.each_with_index do |ele,idx|
+      hashed_str += (idx.to_s + ele.hash.abs.to_s)
+    end
+    hashed_str.to_i
   end
 end
 
 class String
   def hash
+    alpha = ("a".."z").to_a
+    letters = self.split("")
+    nums = letters.map{|ele| alpha.index(ele)}
+    nums.hash
   end
 end
 
@@ -16,6 +25,12 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    hash_arr = []
+    self.each do |k,v|
+      hash_arr << k.hash
+      hash_arr << v.hash
+    end
+    hash_arr.sort.hash
+    
   end
 end
